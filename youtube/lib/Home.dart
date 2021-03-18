612eef797da08bb83b231be94178a90384c6266c
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2021-03-17 21:43:07 -0300

    criando a appBar do app

--- youtube/lib/Home.dart
+++ youtube/lib/Home.dart
@@ -9,7 +9,23 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Image.asset(
+            "images/youtube.png",
+          width: 98,
+          height: 22,
+        ),
+        //leading: Image.asset("images/youtube.png"),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.videocam), onPressed: (){}),
+          IconButton(icon: Icon(Icons.search), onPressed: (){}),
+          IconButton(icon: Icon(Icons.account_circle), onPressed: (){})
+        ],
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Colors.grey
+        ),
+      ),
       body: Container(
 
       ),
--- youtube/lib/main.dart
+++ youtube/lib/main.dart
@@ -5,5 +5,6 @@ import 'Home.dart';
 void main() {
   runApp(MaterialApp(
     home: Home(),
+    debugShowCheckedModeBanner: false,
   ));
 }
